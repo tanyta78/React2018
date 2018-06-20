5ee70b466eaf276c2fd22ffb690f1b790c96bf2b
Tatyana Milanova <tanyta78@gmail.com>
2018-06-20 22:29:24 +0300

    sample react web app - step1 - step 8
    Udilia - https://udilia.com/courses/learn-react-by-building-a-web-app
    build React applications from scratch

---
+++ UdiliaDemo/learn-react-by-building-a-web-app-master/react-coin/src/components/list/List.js
@@ -0,0 +1,70 @@
+import React, { Component } from 'react';
+import { handleResponse } from '../../helpers';
+import { API_URL } from '../../config';
+import Loading from '../common/Loading';
+import Table from './Table';
+
+class List extends Component {
+	constructor() {
+		super();
+
+		this.state = {
+			loading: false,
+			currencies: [],
+			error: null,
+		};
+	}
+
+	componentDidMount() {
+		this.setState({ loading: true });
+
+		fetch(`${API_URL}/cryptocurrencies?page=1&perPage=20`)
+			.then(handleResponse)
+			.then((data) => {
+
+				this.setState({
+					currencies: data.currencies, loading: false
+				});
+			})
+			.catch((error) => {
+				this.setState({
+					error: error.errorMessage,
+					loading: false
+				});
+
+			});
+	}
+
+	renderChangePercent(percent) {
+		if (percent > 0) {
+			return <span className="percent-raised">{percent}% &uarr;</span>;
+		} else if (percent < 0) {
+			return <span className="percent-fallen">{percent}% &darr;</span>;
+		} else {
+			return <span>{percent}</span>;
+		}
+	}
+
+	render() {
+		const { loading, error, currencies } = this.state;
+
+		//render only loding component, if loading state is set to true
+		if (loading) {
+			return <div className="loading-container"><Loading /></div>;
+		}
+
+		//render only error message , if error occure while fetching data
+		if (error) {
+			return <div className="error">{error}</div>;
+		}
+
+		return (
+			<Table
+				currencies={currencies}
+				renderChangePercent={this.renderChangePercent}
+			/>
+		);
+	}
+}
+
+export default List;
